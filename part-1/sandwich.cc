4e0082945ae69fa7ba57bbedee212bc79102f528
Jackson Cao <jacksoncao@csu.fullerton.edu>
2023-10-30 10:21:44 -0700

    fix linter problems

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -8,7 +8,7 @@
 
 int main(int argc, char* argv[]) {
   if (argc != 4) {
-    std::cerr << "Error: You must supply three arguments." << std::endl;
+    std::cerr << "error: You must supply three arguments" << std::endl;
     return 1;
   }
 
