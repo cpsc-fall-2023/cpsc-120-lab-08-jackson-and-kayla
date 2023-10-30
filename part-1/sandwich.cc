4087defbe0ad35018ef66e782a556faecc524c0c
Jackson Cao <jacksoncao@csu.fullerton.edu>
2023-10-30 10:28:36 -0700

    fixed linter problems

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -5,20 +5,20 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(int argc, char* argv[]) {
-  if (argc != 4) {
-    std::cerr << "error: You must supply three arguments" << std::endl;
+  std::vector<std::string> arguments(argv, argv + argc);
+  if (arguments.size() != 4) {
+    std::cout << "error: you must supply three arguments\n";
     return 1;
   }
-
-  std::string protein = argv[1];
-  std::string bread = argv[2];
-  std::string condiment = argv[3];
-
-  std::cout << "Your order:\n"
+  std::string protein{arguments.at(1)};
+  std::string bread{arguments.at(2)};
+  std::string condiment{arguments.at(3)};
+  std::cout << "Your order: \n"
             << "A " << protein << " sandwich on " << bread << " with "
-            << condiment << "." << std::endl;
-
+            << condiment << "."
+            << "\n";
   return 0;
 }
