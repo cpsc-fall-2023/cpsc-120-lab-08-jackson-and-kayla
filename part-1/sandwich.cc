8608af829911139ea5fdae139fff14eca5dea209
Jackson Cao <jacksoncao@csu.fullerton.edu>
2023-10-30 10:19:55 -0700

    fix linter

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -5,19 +5,18 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> arguments{argv, argv + argc};
-
   if (argc != 4) {
-    std::cerr << "error: you must supply three arguments" << std::endl;
+    std::cerr << "Error: You must supply three arguments." << std::endl;
     return 1;
   }
+
   std::string protein = argv[1];
   std::string bread = argv[2];
   std::string condiment = argv[3];
-  std::cout << "Your order: \n"
+
+  std::cout << "Your order:\n"
             << "A " << protein << " sandwich on " << bread << " with "
             << condiment << "." << std::endl;
 
