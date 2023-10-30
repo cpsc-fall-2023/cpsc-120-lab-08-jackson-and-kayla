b984f33c5ac3a395f0d09d9d3aa77b2e12a537bd
Jackson Cao <jacksoncao@csu.fullerton.edu>
2023-10-30 10:57:14 -0700

    removed intructions to fix linting issues

--- part-2/average.cc
+++ part-2/average.cc
@@ -10,21 +10,11 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // Validate that there is at least one command line argument.
-  // If not, print an error message and return a non-zero value.
   if (arguments.size() < 2) {
     std::cout << "error: you must supply at least one number\n";
     return 1;
   }
 
-  // Write a for-each loop to sum (add up) all of the command line
-  // arguments.
-  // Use a double or float type so that your program preserves fractional
-  // values.
-  // The loop needs to skip over the command name, which is the first element
-  // of the arguments vector.
-  // Each argument is a std::string. You will need to convert each string into
-  // a number with the std::stod or std::stof function.
   double total{0};
   int n{0};
   bool first{true};
@@ -38,14 +28,8 @@ int main(int argc, char* argv[]) {
     ++n;
   }
 
-  // After the loop has finished summing the arguments, calculate the
-  // average of the values. Recall that the average is the total value divided
-  // by the number of values.
   double average = total / n;
 
-  // Use cout to print out a message of the form
-  // average = *AVERAGE*
-  // on its own line.
   std::cout << "average = " << average << "\n";
 
   return 0;
